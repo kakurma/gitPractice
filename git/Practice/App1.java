bcc7010745ed73e7f45dd8d2c2db203c458d4854
kakurma <krmadhava@gmail.com>
2023-09-30 15:24:59 +0200

    added app1 in b2

---
+++ git/Practice/App1.java
@@ -0,0 +1,20 @@
+package git.Practice;
+
+public class App1{
+
+    public static void main( String[] args)
+{
+
+    System.out.println("Hellow world");
+}
+    public void method1(){
+        System.out.println("method1");
+    }
+     public void method2(){
+        System.out.println("method1");
+    }
+      public void method3(){
+        System.out.println("method1");
+    }
+
+}
