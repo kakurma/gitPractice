6ee63f787428b9f80dcdb244f9c830f0b140cd82
kakurma <krmadhava@gmail.com>
2023-09-30 11:46:04 +0200

    first commit

---
+++ git/Practice/App.java
@@ -0,0 +1,14 @@
+package git.Practice;
+
+public class App{
+
+    public static void main( String[] args)
+{
+
+    System.out.println("Hellow world");
+}
+    public void method1(){
+        System.out.println("method1");
+    }
+
+}
